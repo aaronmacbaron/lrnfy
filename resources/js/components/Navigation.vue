e84e7d8994583b6f169b7f39bc5268a37cae53ac
Aaron Pedro <aaron.jay.pedro@gmail.com>
2020-07-27 21:32:03 -0600

    add base navigation component

---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <router-link :to="{name:'home'}">Home</router-link>
+            </li>
+            <li>
+                <router-link :to="{name:'about'}">About</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
