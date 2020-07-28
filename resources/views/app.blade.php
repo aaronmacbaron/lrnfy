2145f8c14f37b8bfd6b8c100ea488243b71e6c60
Aaron Pedro <aaron.jay.pedro@gmail.com>
2020-07-27 21:32:51 -0600

    add routing and actual landing page for spa

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lrnfy</title>
+</head>
+<body class="antilaliased font-sans bg-gray-200">
+    <main id="app">
+        <navigation></navigation>
+        <div class="py-5">
+            <router-view></router-view>
+        </div>
+    </main>
+    <script src="/js/app.js"></script>
+</body>
+</html>
